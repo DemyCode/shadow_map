b7564af986e68723a8a8be841b833295ed4c8612
Mehdi <mehdi.bekhtaoui@epita.fr>
2020-06-20 20:05:54 +0200

    Basic displacement in 3D

--- src/main.cpp
+++ src/main.cpp
@@ -11,36 +11,36 @@
 #include "glutils/program.hh"
 #include "glutils/object.hh"
 
-// renderQuad() renders a 1x1 XY quad in NDC
-// -----------------------------------------
-unsigned int quadVAO = 0;
-unsigned int quadVBO;
-void renderQuad()
-{
-    if (quadVAO == 0)
-    {
-        float quadVertices[] = {
-                // positions        // texture Coords
-                -1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
-                -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
-                1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
-                1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
-        };
-        // setup plane VAO
-        glGenVertexArrays(1, &quadVAO);
-        glGenBuffers(1, &quadVBO);
-        glBindVertexArray(quadVAO);
-        glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(1);
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-    }
-    glBindVertexArray(quadVAO);
-    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-    glBindVertexArray(0);
-}
+//// renderQuad() renders a 1x1 XY quad in NDC
+//// -----------------------------------------
+//unsigned int quadVAO = 0;
+//unsigned int quadVBO;
+//void renderQuad()
+//{
+//    if (quadVAO == 0)
+//    {
+//        float quadVertices[] = {
+//                // positions        // texture Coords
+//                -1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
+//                -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+//                1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
+//                1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+//        };
+//        // setup plane VAO
+//        glGenVertexArrays(1, &quadVAO);
+//        glGenBuffers(1, &quadVBO);
+//        glBindVertexArray(quadVAO);
+//        glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+//        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+//        glEnableVertexAttribArray(0);
+//        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+//        glEnableVertexAttribArray(1);
+//        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+//    }
+//    glBindVertexArray(quadVAO);
+//    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+//    glBindVertexArray(0);
+//}
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -52,9 +52,6 @@ float lastTime = 0.0f;
 Camera camera = Camera(glm::vec3(0.0f, 0.0f,  3.0f),
                        glm::vec3(0.0f, 0.0f,  -1.0f),
                        glm::vec3(0.0f, 1.0f,  0.0f));
-Camera camera_light = Camera(glm::vec3(0.0f, 0.0f,  3.0f),
-                             glm::vec3(0.0f, 0.0f,  -1.0f),
-                             glm::vec3(0.0f, 1.0f,  0.0f));
 
 void processInputs(GLFWwindow* window)
 {
@@ -119,36 +116,6 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 
 void drawing(Program program, Object object1, Object object2, Object object3)
 {
-    // Drawing
-
-    // create transformations
-    glm::mat4 model1 = glm::mat4(1.0f);
-    model1 = glm::translate(model1, glm::vec3(0.0f, 0.5f, 0.0f));
-    // retrieve the matrix uniform locations
-    unsigned int modelLoc = glGetUniformLocation(program.id_, "model");
-    // pass them to the shaders (3 different ways)
-    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model1));
-    object1.render();
-
-    // create transformations
-    glm::mat4 model2 = glm::mat4(1.0f);
-    model2 = glm::rotate(model2, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-    model2 = glm::translate(model2, glm::vec3(5.0f, 1.0f, 3.0f));
-    // retrieve the matrix uniform locations
-    unsigned int modelLoc2 = glGetUniformLocation(program.id_, "model");
-    // pass them to the shaders (3 different ways)
-    glUniformMatrix4fv(modelLoc2, 1, GL_FALSE, glm::value_ptr(model2));
-    object2.render();
-
-    // create transformations
-    glm::mat4 model3 = glm::mat4(1.0f);
-    model3 = glm::translate(model3, glm::vec3(0.0, -50.0, 0.0));
-    model3 = glm::scale(model3, glm::vec3(100.0, 100.0, 100.0));
-    // retrieve the matrix uniform locations
-    unsigned int modelLoc3 = glGetUniformLocation(program.id_, "model");
-    // pass them to the shaders (3 different ways)
-    glUniformMatrix4fv(modelLoc3, 1, GL_FALSE, glm::value_ptr(model3));
-    object3.render();
 }
 
 int main()
@@ -193,6 +160,10 @@ int main()
     Shader debug_vertex = Shader("../src/shaders/quad_debug/vertex_quad.glsl", 0);
     Shader debug_fragment = Shader("../src/shaders/quad_debug/fragment_quad.glsl", 1);
     Program debug_program = Program(debug_vertex, debug_fragment);
+
+    Shader dummy_vertex = Shader("../src/shaders/basic/vertex.glsl", 0);
+    Shader dummy_fragment = Shader("../src/shaders/basic/fragment.glsl", 1);
+    Program dummy_program = Program(dummy_vertex, dummy_fragment);
 
     float vertices[] = {
             // positions          // colors
@@ -344,6 +315,7 @@ int main()
 
     // configure depth map FBO
     // -----------------------
+    /*
     const unsigned int SHADOW_WIDTH = 1024, SHADOW_HEIGHT = 1024;
     unsigned int depthMapFBO;
     glGenFramebuffers(1, &depthMapFBO);
@@ -362,16 +334,16 @@ int main()
     glDrawBuffer(GL_NONE);
     glReadBuffer(GL_NONE);
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
+    */
     // shader configuration
     // --------------------
-    program.use();
-    glUniform1i(glGetUniformLocation(program.id_, "shadowMap"), 1);
+    dummy_program.use();
+    // glUniform1i(glGetUniformLocation(program.id_, "shadowMap"), 1);
 
-    debug_program.use();
-    glUniform1i(glGetUniformLocation(program.id_, "depthMap"), 0);
+    //debug_program.use();
+    //glUniform1i(glGetUniformLocation(program.id_, "depthMap"), 0);
 
-    glm::vec3 lightPos(-2.0f, 4.0f, -1.0f);
+    //glm::vec3 lightPos(-2.0f, 4.0f, -1.0f);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -380,11 +352,11 @@ int main()
         glfwSetCursorPosCallback(window, mouse_callback);
 
         // Clearning
-        // glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glEnable(GL_DEPTH_TEST);
 
-
+/*
         // FIRST RENDERING FOR LIGHT DEPTH
         glm::mat4 lightProjection, lightView;
         glm::mat4 lightSpaceMatrix;
@@ -404,32 +376,63 @@ int main()
         // End of Drawing
         glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
-
+*/
         // NORMAL RENDERING WITH THE USE OF DEPTHMAPFBO
         glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        program.use();
+        dummy_program.use();
         glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
         glm::mat4 view  = camera.get_view();
-        unsigned int viewLoc  = glGetUniformLocation(program.id_, "view");
-        unsigned int projectionLoc  = glGetUniformLocation(program.id_, "projection");
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+        unsigned int viewLoc  = glGetUniformLocation(dummy_program.id_, "view");
+        unsigned int projectionLoc  = glGetUniformLocation(dummy_program.id_, "projection");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
         glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
         // set light uniforms
-        glUniform3f(glGetUniformLocation(program.id_, "viewPos"), camera.position_.x, camera.position_.y, camera.position_.z);
-        glUniform3f(glGetUniformLocation(program.id_, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
-        glUniformMatrix4fv(glGetUniformLocation(program.id_, "lightSpaceMatrix"), 1, GL_FALSE, glm::value_ptr(lightSpaceMatrix));
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, depthMap);
-        drawing(program, object1, object2, object3);
+        // glUniform3f(glGetUniformLocation(program.id_, "viewPos"), camera.position_.x, camera.position_.y, camera.position_.z);
+        // glUniform3f(glGetUniformLocation(program.id_, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
+        // glUniformMatrix4fv(glGetUniformLocation(program.id_, "lightSpaceMatrix"), 1, GL_FALSE, glm::value_ptr(lightSpaceMatrix));
+//        glActiveTexture(GL_TEXTURE0);
+//        glBindTexture(GL_TEXTURE_2D, depthMap);
+        // drawing(dummy_program, object1, object2, object3);
+
+        // Drawing
+
+        // create transformations
+        glm::mat4 model1 = glm::mat4(1.0f);
+        model1 = glm::translate(model1, glm::vec3(0.0f, 0.5f, 0.0f));
+        // retrieve the matrix uniform locations
+        unsigned int modelLoc = glGetUniformLocation(dummy_program.id_, "model");
+        // pass them to the shaders (3 different ways)
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model1));
+        object1.render();
+
+        // create transformations
+        glm::mat4 model2 = glm::mat4(1.0f);
+        model2 = glm::rotate(model2, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+        model2 = glm::translate(model2, glm::vec3(5.0f, 1.0f, 3.0f));
+        // retrieve the matrix uniform locations
+        unsigned int modelLoc2 = glGetUniformLocation(dummy_program.id_, "model");
+        // pass them to the shaders (3 different ways)
+        glUniformMatrix4fv(modelLoc2, 1, GL_FALSE, glm::value_ptr(model2));
+        object2.render();
+
+        // create transformations
+        glm::mat4 model3 = glm::mat4(1.0f);
+        model3 = glm::translate(model3, glm::vec3(0.0, -50.0, 0.0));
+        model3 = glm::scale(model3, glm::vec3(100.0, 100.0, 100.0));
+        // retrieve the matrix uniform locations
+        unsigned int modelLoc3 = glGetUniformLocation(dummy_program.id_, "model");
+        // pass them to the shaders (3 different ways)
+        glUniformMatrix4fv(modelLoc3, 1, GL_FALSE, glm::value_ptr(model3));
+        object3.render();
 
 
         // OPTIONNAL DRAWING FOR DEBUG
-        debug_program.use();
-        glUniform1f(glGetUniformLocation(program.id_, "near_plane"), near_plane);
-        glUniform1f(glGetUniformLocation(program.id_, "far_plane"), far_plane);
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, depthMap);
+//        debug_program.use();
+//        glUniform1f(glGetUniformLocation(program.id_, "near_plane"), near_plane);
+//        glUniform1f(glGetUniformLocation(program.id_, "far_plane"), far_plane);
+//        glActiveTexture(GL_TEXTURE0);
+//        glBindTexture(GL_TEXTURE_2D, depthMap);
         // drawing(debug_program, object1, object2, object3);
         // renderQuad();
 
